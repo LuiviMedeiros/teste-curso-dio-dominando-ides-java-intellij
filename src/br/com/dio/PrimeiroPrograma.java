5c96950969adc6d7130ea3b3f3667137b7fafa4b
Luivi <luivi9@gmail.com>
2022-07-18 14:49:49 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,7 +6,43 @@ public class PrimeiroPrograma {
     public static void main (String[] args){
         Gato gato = new Gato();
         System.out.println(gato);
-
-
+        Livro Livro = new Livro("O problemas dos 3 corpos",300);
+        System.out.println(Livro);
   }
+}
+class Livro {
+    private String nome;
+    private String npg;
+
+    public Livro(String nome, int i) {
+    }
+
+    public Livro(String nome, String npg) {
+        this.nome = nome;
+        this.npg = npg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNpg() {
+        return npg;
+    }
+
+    public void setNpg(String npg) {
+        this.npg = npg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npg='" + npg + '\'' +
+                '}';
+    }
 }
